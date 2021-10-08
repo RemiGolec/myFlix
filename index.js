c7124c9c10a55bde9c57fc5b6ab4b26aacc1b792
RemiGolec <83162017+RemiGolec@users.noreply.github.com>
2021-10-08 22:45:38 +0100

    no message

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let allowedOrigins =
   [
     'http://localhost:8080',
     'http://testsite.com',
-    'http://localhost:1234',
+    'http://localhost:1234'
   ];
 app.use(cors({
   origin: (origin, callback) => {
